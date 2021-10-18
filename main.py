be321bbc0938b757d2ee0f722eebdaa6d14032d0
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2021-10-18 16:55:08 +0200

    v1.3
    Add image display
    Edit CSS

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+import time
+
 import numpy as np
 from PIL import Image
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
+# Specify the path where the app will store the uploaded files
+UPLOAD_FOLDER = r'static\uploads'
+
 app = Flask(__name__)
+
+# Specify the path where the app will store the uploaded files
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 Bootstrap(app)
 
 
 @app.route("/")
 def hello_world():
-    top_colors = get_colors_stats("test.png")
+    top_colors = get_colors_stats("static/uploads/test.png")
 
     return render_template('index.html', top_colors=top_colors)
 
@@ -33,24 +42,28 @@ def get_colors_stats(image):
     width = image_array.shape[1]
     pixels = height * width
 
-    rgb_occurences = {}
+    # Create a dict to store every single RGB value from the image as the key and the occurrence count as value
+    rgb_occurrences = {}
+    # Create a list to store uniques rgb values as tuples
     all_image_colors = []
 
     for x in range(width):
         for y in range(height):
             rgb = tuple(image_array[y][x])
-            if f"{rgb}" in rgb_occurences:
-                rgb_occurences[f'{rgb}'] += 1
+            if f"{rgb}" in rgb_occurrences:
+                rgb_occurrences[f'{rgb}'] += 1
             else:
-                rgb_occurences[f'{rgb}'] = 1
+                rgb_occurrences[f'{rgb}'] = 1
                 all_image_colors.append(rgb)
 
-    top_10_rgb_occurences = sorted(rgb_occurences, key=rgb_occurences.get, reverse=True)[:10]
+    # Create a new list containing rgb values (dict keys) sorted by occurrence count (dict values)
+    # in descending order (reverse=True)
+    # Sliced to keep only the 10 most common colors
+    top_rgb_occurences = sorted(rgb_occurrences, key=rgb_occurrences.get, reverse=True)[:10]
 
+    # # Create a final list of dicts that will contain the most common colors information
     top_colors = []
-
-    # # Create a list of dicts containing rgb values
-    for color in top_10_rgb_occurences:
+    for color in top_rgb_occurences:
         for col in all_image_colors:
             if color == str(col):
                 top_colors.append({'rgb': col})
@@ -65,10 +78,10 @@ def get_colors_stats(image):
         color['hex'] = f'#{convert_to_hex(r)}{convert_to_hex(g)}{convert_to_hex(b)}'
 
     # Add percentage
-    for color in top_10_rgb_occurences:
+    for color in top_rgb_occurences:
         for c in top_colors:
             if color == str(c['rgb']):
-                c['rate'] = round(rgb_occurences[color] / pixels * 100, 2)
+                c['rate'] = round(rgb_occurrences[color] / pixels * 100, 2)
 
     return top_colors
 
