cb06e538bfe15c7e9067e91f2cae221db210110a
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2021-10-18 13:42:44 +0200

    v1.2
    Add color percentage

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import numpy as np
 from PIL import Image
 
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
 @app.route("/")
 def hello_world():
-    top_colors = get_image_colors("sample.jpg")
-
-    print(top_colors)
+    top_colors = get_colors_stats("test.png")
 
     return render_template('index.html', top_colors=top_colors)
 
@@ -23,17 +23,15 @@ def convert_to_hex(value):
     return hex_value
 
 
-def get_image_colors(image):
+def get_colors_stats(image):
     im = Image.open(image)
 
     # Convert image into NumPy array
     image_array = np.array(im)
-    # height x width x channel
-    # print(image_array.shape)
-    # print(image_array.ndim)
-
+    # Shape = height x width x channels
     height = image_array.shape[0]
     width = image_array.shape[1]
+    pixels = height * width
 
     rgb_occurences = {}
     all_image_colors = []
@@ -50,11 +48,14 @@ def get_image_colors(image):
     top_10_rgb_occurences = sorted(rgb_occurences, key=rgb_occurences.get, reverse=True)[:10]
 
     top_colors = []
-    for color in all_image_colors:
-        if str(color) in top_10_rgb_occurences:
-            # top_colors.append(color)
-            top_colors.append({'rgb': color})
 
+    # # Create a list of dicts containing rgb values
+    for color in top_10_rgb_occurences:
+        for col in all_image_colors:
+            if color == str(col):
+                top_colors.append({'rgb': col})
+
+    # Add hex values to the list of dicts
     for color in top_colors:
         rgb = color['rgb']
         r = rgb[0]
@@ -62,6 +63,12 @@ def get_image_colors(image):
         b = rgb[2]
 
         color['hex'] = f'#{convert_to_hex(r)}{convert_to_hex(g)}{convert_to_hex(b)}'
+
+    # Add percentage
+    for color in top_10_rgb_occurences:
+        for c in top_colors:
+            if color == str(c['rgb']):
+                c['rate'] = round(rgb_occurences[color] / pixels * 100, 2)
 
     return top_colors
 
