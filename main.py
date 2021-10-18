5537fa849794aed56146f7a7a29466cbd4cde7ea
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2021-10-18 11:31:45 +0200

    v1.1
    Add hex color values.
    Add color display in HTML.

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import numpy as np
 from PIL import Image
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+from flask import Flask, render_template
 
-    im = Image.open("test.png")
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    top_colors = get_image_colors("sample.jpg")
+
+    print(top_colors)
+
+    return render_template('index.html', top_colors=top_colors)
+
+
+def convert_to_hex(value):
+    hex_value = hex(value).replace('0x', '')
+    # Added to uniformize output with 2 digits.
+    if len(hex_value) < 2:
+        hex_value = f'0{hex_value}'
+    return hex_value
+
+
+def get_image_colors(image):
+    im = Image.open(image)
 
     # Convert image into NumPy array
     image_array = np.array(im)
@@ -15,17 +35,37 @@ if __name__ == '__main__':
     height = image_array.shape[0]
     width = image_array.shape[1]
 
-    color = {}
+    rgb_occurences = {}
+    all_image_colors = []
 
     for x in range(width):
         for y in range(height):
             rgb = tuple(image_array[y][x])
-            if f"{rgb}" in color:
-                color[f'{rgb}'] += 1
+            if f"{rgb}" in rgb_occurences:
+                rgb_occurences[f'{rgb}'] += 1
             else:
-                color[f'{rgb}'] = 1
+                rgb_occurences[f'{rgb}'] = 1
+                all_image_colors.append(rgb)
 
-    top_10 = sorted(color, key=color.get, reverse=True)[:10]
+    top_10_rgb_occurences = sorted(rgb_occurences, key=rgb_occurences.get, reverse=True)[:10]
 
-    top_10_colors = [key for key in top_10]
-    print(top_10_colors)
+    top_colors = []
+    for color in all_image_colors:
+        if str(color) in top_10_rgb_occurences:
+            # top_colors.append(color)
+            top_colors.append({'rgb': color})
+
+    for color in top_colors:
+        rgb = color['rgb']
+        r = rgb[0]
+        g = rgb[1]
+        b = rgb[2]
+
+        color['hex'] = f'#{convert_to_hex(r)}{convert_to_hex(g)}{convert_to_hex(b)}'
+
+    return top_colors
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run(debug=True)
